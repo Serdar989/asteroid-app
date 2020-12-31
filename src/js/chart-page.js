8f0e342b79de1c6673db03d64094945c7eae7dae
Serdar989 <vojvodaserdar@gmail.com>
2020-12-31 18:25:07 +0100

    Initial commit

---
+++ src/js/chart-page.js
@@ -0,0 +1,47 @@
+import CHARTUI from './views/viewChart';
+import CHARTDATA from './models/Chart';
+import { state } from './models/Chart';
+import '../scss/style.scss';
+import { elements } from './views/base';
+import { api_key } from './config';
+
+const controlChartPage = async function () {
+  const asteroid = new CHARTDATA();
+  const ui = new CHARTUI();
+  let urls = [];
+  let showChartData;
+  // ui.showLoader();
+
+  if (asteroid.idList.length === 0) {
+    ui.showMessage();
+  } else {
+    for (let i = 0; i < asteroid.idList.length; i++) {
+      urls.push(
+        `http://www.neowsapp.com/rest/v1/neo/${asteroid.idList[i]}?api_key=${api_key}`
+      );
+    }
+    try {
+      state.chartDataArr = await asteroid.asteroidDataArr(urls);
+
+      showChartData = asteroid.getChartData(state.chartDataArr);
+
+      ui.showChart(showChartData);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+};
+
+// Display local storage
+// document.addEventListener('DOMContentLoaded', () => {
+//   controlChartPage();
+// });
+document.addEventListener('readystatechange', (event) => {
+  const ui = new CHARTUI();
+
+  if (event.target.readyState === 'interactive') {
+    ui.showLoader();
+  } else if (event.target.readyState === 'complete') {
+    controlChartPage();
+  }
+});
---
+++ src/js/config.js
@@ -0,0 +1,1 @@
+export const api_key = 'x0HeIJzRCLm3lj0zrfXt2LltusKVCO7aoHmRkVq2';
